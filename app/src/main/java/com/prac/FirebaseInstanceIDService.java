da886e09a4a325c22a8f783acf389d2a69258577
nwh1124 <nwh0716@gmail.com>
2021-04-19 09:14:42 +0900

    안드로이드푸시

--- app/src/main/java/com/prac/FirebaseInstanceIDService.java
+++ app/src/main/java/com/prac/FirebaseInstanceIDService.java
@@ -1,7 +1,5 @@
 package com.prac;
 
-import android.app.Service;
-import android.support.v4.app.INotificationSideChannel;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -13,9 +11,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GetTokenResult;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
-import com.google.firebase.iid.internal.FirebaseInstanceIdInternal;
 import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.firebase.messaging.FirebaseMessagingService;
 
 
 public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
